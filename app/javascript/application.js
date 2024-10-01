0016720a7c997fd6b473f4bb68cf7050815487df
Michael Burton <michael.burton@entangla.com>
2024-10-01 15:29:25 -0700

    sb-admin 2 theme

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jQuery"
+import "jQuery.easing"
+import "bootstrap"
+import "sb-admin-2"
+import "chartjs"
+import "chart-area-demo"
+import "chart-pie-demo"
+
